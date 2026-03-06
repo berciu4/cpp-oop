56f7e117980f8e643e4a1e0690f9dab414db231b
berciu4 <berciuantonio42@gmail.com>
2026-03-06 23:11:43 +0200

    Update main.cpp

--- lectia01_introductiva/exercitiu2_erbivor/main.cpp
+++ lectia01_introductiva/exercitiu2_erbivor/main.cpp
@@ -22,18 +22,40 @@ using namespace std;
 //     void afiseazaStare() { ... }
 // };
 
+class Erbivor {
+private:
+    string nume;
+    double energie;
+public:
+    Erbivor(string n, double e) {
+        nume = n;
+        energie = e;
+    }
+
+    void mananca(double cat) {
+        energie = energie + cat / 10;
+    }
+
+    void afiseazaStare() {
+        cout << "Erbivor-ul " << nume << " are energie " << energie << endl;
+    }
+};
+
 int main() {
     // TODO: Creează un erbivor "Bambi" cu energie 50
     // Exemplu: Erbivor animal("Bambi", 50.0);
+    Erbivor animal("Bambi", 50.0)
 
     // TODO: Afișează starea (ar trebui: "Erbivor-ul Bambi are energie 50")
     // Exemplu: animal.afiseazaStare();
+    animal.afiseazaStare();
 
     // TODO: Mănâncă 100 (energia crește cu 10, deci devine 60)
     // Exemplu: animal.mananca(100.0);
-
+    animal.mananca(100.0)
     // TODO: Afișează starea din nou (ar trebui: "Erbivor-ul Bambi are energie 60")
     // Exemplu: animal.afiseazaStare();
+        animal.afiseazaStare();
 
     return 0;
 }
